15abab9806e73eaa02c8c051175bf597c650008c
Jason Rothgeb <jrothgeb@gmail.com>
2022-07-07 19:00:51 -0500

    commit tree ad2e45240de277fa9925691aea1aea8cf0862240

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'susie'
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+// console.log(upperCaseCustomerName('bob'));
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'phil';
+    return leastFavoriteCustomer;
+}
+
+
